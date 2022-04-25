3209a30ce888fb54ef41ca2628d31a84845f2ab4
Evandleclair <evandleclair@gmail.com>
2022-04-25 15:14:10 -0400

    Removed rectangle scaling as it was overcomplicating things. System now relies entirely on font scaling.

--- ASCIIFORMAPP/ImageTakerMethods.cs
+++ ASCIIFORMAPP/ImageTakerMethods.cs
@@ -30,45 +30,7 @@ namespace ASCIIFormApp
                             f.GdiCharSet,
                             f.GdiVerticalFont);
         }
-        internal RectangleF GetScaledRect(Graphics g, RectangleF r, float xScale, float yScale)
-        {
-            Console.WriteLine((float)r.X);
-            Console.WriteLine((float)r.Y);
-            Console.WriteLine((float)r.Width);
-            Console.WriteLine((float)r.Height);
-            Console.WriteLine((float)(r.X * xScale));
-            Console.WriteLine((float)(r.Y * yScale));
-            Console.WriteLine((float)(r.Width * xScale));
-            Console.WriteLine((float)(r.Height * yScale));
-
-            return new RectangleF((float)(r.X * xScale),
-                                (float)(r.Y * yScale),
-                                (float)(r.Width * xScale),
-                                (float)(r.Height * yScale));
-            /*
-             *  return new Rectangle((int)Math.Ceiling(r.X * xScale),
-                                (int)Math.Ceiling(r.Y * yScale),
-                                (int)Math.Ceiling(r.Width * xScale),
-                                (int)Math.Ceiling(r.Height * yScale));
-             */
-        }
-        internal Font GenerateFont (DataHandler myHandler, Graphics g, float hScaleRatio, float wScaleRatio)
-        {
-            //string firstlineofASCII = myHandler.heldASCIIImage.Substring(0, myHandler.heldASCIIImage.IndexOf(Environment.NewLine));
-
-
-            /*
-            float ScaleRatio = (hScaleRatio < wScaleRatio)
-                ? ScaleRatio = hScaleRatio
-                : ScaleRatio = wScaleRatio;
-
-
-            float ScaleFontSize = 12 * ScaleRatio;
-            return new Font(FontFamily.GenericMonospace, ScaleFontSize);
-            */
-            return new Font(FontFamily.GenericMonospace, 12, FontStyle.Regular);
-
-        }
+      
         internal rawTextFile LoadImageDialog(DataHandler dataHandler)
         {
             try{
